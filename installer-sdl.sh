3536c20af648377c9490bfbeaf29a019dd2e30be
spartrekus <spartrekus@gmail.com>
2018-11-24 21:01:48 +0100

    Add files via upload

---
+++ installer-sdl.sh
@@ -0,0 +1,15 @@
+
+
+
+
+apt-get install nasm libsdl-mixer1.2 libsdl1.2-dev  libsdl-image1.2
+
+
+exit
+
+
+ii  libsdl-image1.2:armhf                 1.2.12-5+deb9u1                      armhf        Image loading library for Simple DirectMedia Layer 1.2, libraries
+ii  libsdl-mixer1.2:armhf                 1.2.12-11+b1                         armhf        Mixer library for Simple DirectMedia Layer 1.2, libraries
+hi  libsdl1.2-dev                         1.2.15+dfsg1-6rpi                    armhf        Simple DirectMedia Layer development files
+ii  libsdl1.2debian:armhf                 1.2.15+dfsg1-6rpi                    armhf        Simple DirectMedia Layer
+ii  libsdl2-2.0-0:armhf                   2.0.5+dfsg1-2                        armhf        Simple DirectMedia Layer
